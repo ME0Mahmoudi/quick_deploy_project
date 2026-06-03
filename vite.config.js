6a89b0cb3531d52a53cd71cb80f0b544b8d92768
ME0Mahmoudi <ME0Mahmoudi@gmail.com>
2026-06-03 19:29:55 +0330

    added conditional base path to support both github pages deployment and cloudflare pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/quick_deploy_project/",
+  base: process.env.GITHUB_PAGES
+    ? '/quick_deploy_project/'
+    : '/',
   plugins: [react()],
 })
